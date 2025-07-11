4da546bf40dc0ed0d647b9fd7767a1d553208914
ARUNAGIRINATHAN_K <167340745+arun-techverse@users.noreply.github.com>
2025-07-11 13:00:29 +0000

    login-class

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,3 +3,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 from datetime import datetime
+
+class LoginPage:
+    def __init__(self, driver, config):
+        self.driver = driver
+        self.config = config
+        self.url = config['DEFAULT']['base_url']
+        self.username_field = (By.ID, "user-name")
+        self.password_field = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
+        self.error_message = (By.CSS_SELECTOR, "h3[data-test='error']")
